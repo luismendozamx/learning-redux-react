def3cca99bab08730eb3a1e5530db1c6577bf015
Luis Mendoza <luis@mendoza.io>
2016-03-15 14:03:16 -0600

    Changes tabs for spaces

--- intro-redux/ex1/index.js
+++ intro-redux/ex1/index.js
@@ -6,24 +6,24 @@ var store = createStore(counter);
 
 // Reducer function
 function counter (state, action) {
-	if (typeof state === 'undefined') {
-		return 0;
-	}
+  if (typeof state === 'undefined') {
+    return 0;
+  }
 
-	switch(action.type) {
-		case 'INC':
-			return state + 1;
-		case 'DEC':
-			return state - 1;
-		default:
-			return state;
-	}
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    default:
+      return state;
+  }
 };
 
 // render function
 function render () {
-	//document.body.innerText = store.getState();
-	console.log(store.getState());
+  //document.body.innerText = store.getState();
+  console.log(store.getState());
 };
 
 // call back every time and action is dispatched
@@ -34,5 +34,5 @@ render();
 
 // bind click to dispatch event
 document.addEventListener('click', function() {
-	store.dispatch({ type: 'INC' });
+  store.dispatch({ type: 'INC' });
 });
