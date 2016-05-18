6d1daf34a76f9cc44fa3be8b7de6bb8c43cfbd17
Luis Mendoza <luis@mendoza.io>
2016-05-18 00:14:15 -0500

    Setup components and routes

---
+++ learn-redux/client/components/Photogrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Photogrid extends React.Component{
+  render() {
+    return(
+      <div className="photo-grid">
+        Photo
+      </div>
+    )
+  }
+};
+
+export default Photogrid;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,4 +4,21 @@ import { render } from 'react-dom';
 // Styles
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'));
+// React Router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// Components
+import Main from './components/Main';
+import Single from './components/Single';
+import Photogrid from './components/Photogrid';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Photogrid}></IndexRoute>
+      <Route path="/view/:post_id" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
