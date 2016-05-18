79c0bc15b23764725753c27cccf60ee971a41906
Luis Mendoza <luis@mendoza.io>
2016-05-17 23:54:42 -0500

    Add babelrc, eslintrc and wire React

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Styles
+import css from './styles/style.styl';
+
+render(<p>hello</p>, document.getElementById('root'));
